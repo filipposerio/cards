40467612e3b5f008c155cab374b1fc152efbc50d
filipposerio <filippo.serio@gmail.com>
2016-05-10 14:19:07 +0200

    add mesaage

---
+++ api/controllers/CardmessagesController.js
@@ -0,0 +1,11 @@
+/**
+ * CardmessagesController
+ *
+ * @description :: Server-side logic for managing cardmessages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
